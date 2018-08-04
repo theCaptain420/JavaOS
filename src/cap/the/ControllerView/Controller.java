71cac91e3b0957156a0248ef01b11235b64e8501
Captain <mads130f@edu.easj.dk>
2018-08-04 17:38:29 +0200

    ok

--- src/cap/the/ControllerView/Controller.java
+++ src/cap/the/ControllerView/Controller.java
@@ -3,10 +3,14 @@ package cap.the.ControllerView;
 
 import cap.the.Home.Wallpaper;
 import cap.the.Other.WindowSize;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import javafx.scene.web.WebEngine;
+import javafx.scene.web.WebView;
 
 import java.io.File;
 import java.net.URL;
@@ -23,6 +27,33 @@ public class Controller implements Initializable {
     @FXML
     private ImageView imgV_wallpaper;
 
+    //web
+    @FXML
+    private WebView webView;
+
+    @FXML
+    private Button btn_goto;
+
+    @FXML
+    private javafx.scene.control.TextField txtfield_webs;
+
+
+
+    @FXML
+    private void handleButtonAction(ActionEvent e){
+        if(e.getSource()==btn_goto){
+            System.out.println("test btn");
+            WebEngine webEngine = webView.getEngine();
+            String url = "www.google.com";
+            try {
+                url = txtfield_webs.toString();
+            }catch (Exception ex){
+                ex.printStackTrace();
+            }
+            webEngine.load(url);
+
+        }
+    }
 
     //wallpaper
     @Override
